22fa38d42729428813da655c147e4f0e68fe3c07
Valerio Setti <valerio.setti@gmail.com>
2024-12-02 15:25:03 +0100

    uboot: select NFS v3
    NFS v4 is extremely slow then NFS server is stopped
    and restarted because the target does not renew its
    ID so we need to wait for the grace period to expire
    (that's usually 90 seconds).
    
    Signed-off-by: Valerio Setti <valerio.setti@gmail.com>

--- board/car-player/boot.cmd
+++ board/car-player/boot.cmd
@@ -4,7 +4,7 @@ setenv serverip 192.168.3.100
 
 setenv kernel_filename "Image"
 setenv fdt_filename "meson-gxbb-odroidc2.dtb"
-setenv bootargs "console=ttyAML0,115200n8 root=/dev/nfs ip=dhcp nfsroot=${serverip}:/run/user/1000/nfs,tcp,nfsvers=4 earlyprintk rw rootwait usbcore.autosuspend=-1 fbcon=map:2"
+setenv bootargs "console=ttyAML0,115200n8 root=/dev/nfs ip=dhcp nfsroot=${serverip}:/run/user/1000/nfs,tcp,nfsvers=3 earlyprintk rw rootwait usbcore.autosuspend=-1 fbcon=map:2"
 
 echo > Loading Kernel...
 tftp ${kernel_addr_r} ${kernel_filename}
