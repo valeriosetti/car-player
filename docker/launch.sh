cfe0de3ae8ea0fd2aaa31bf52975a5a50ad2538a
Valerio Setti <valerio.setti@gmail.com>
2025-01-22 14:01:19 +0100

    update github workflow file
    Signed-off-by: Valerio Setti <valerio.setti@gmail.com>

--- docker/launch.sh
+++ docker/launch.sh
@@ -6,8 +6,8 @@ PRJ_ROOT=$SCRIPT_PATH/..
 
 if [ "$COMMAND" != "" ]; then
     # Start docker and launch the specified command (no interactive shell)
-    docker run --rm -v$PRJ_ROOT:/home/br-user --user br-user buildroot:latest $COMMAND
+    docker run --rm -v$PRJ_ROOT:/home/br-user --user $(id -u) buildroot:latest $COMMAND
 else
     # Start docker with interactive shel
-    docker run -it --rm -v$PRJ_ROOT:/home/br-user --user br-user buildroot:latest
+    docker run -it --rm -v$PRJ_ROOT:/home/br-user --user $(id -u) buildroot:latest
 fi
