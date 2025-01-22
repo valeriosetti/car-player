7fc6b1da6564df89d747b58da6f695fa19917df2
Valerio Setti <valerio.setti@gmail.com>
2025-01-22 17:07:14 +0100

    make entries in external.mk explicit
    Signed-off-by: Valerio Setti <valerio.setti@gmail.com>

--- external.mk
+++ external.mk
@@ -1,1 +1,4 @@
-include $(sort $(wildcard $(BR2_EXTERNAL_CAR_PLAYER_PATH)/package/*/*.mk))
+include $(BR2_EXTERNAL_CAR_PLAYER_PATH)/package/dabon-cli/dabon-cli.mk
+include $(BR2_EXTERNAL_CAR_PLAYER_PATH)/package/devmem2/devmem2.mk
+include $(BR2_EXTERNAL_CAR_PLAYER_PATH)/package/hidraw-touch/hidraw-touch.mk
+include $(BR2_EXTERNAL_CAR_PLAYER_PATH)/package/dabon-player/dabon-player.mk
